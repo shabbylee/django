6fda5fd83fd2af93578f9c59f5c700f387ba7253
shawn.li <100818616@qq.com>
2017-05-19 16:38:44 +0800

    add django code

---
+++ guest/sign/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        if username == 'admin' and password == 'admin12345':
+            response = HttpResponseRedirect('/event_manage/')
+            # response.set_cookie('user', username, 120)
+            request.session['user'] = username
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+@login_required
+def event_manage(request):
+    # username = request.COOKIES.get('user', '')
+    username = request.session.get('user', '')
+    return render(request, 'event_manage.html', {'user': username})
